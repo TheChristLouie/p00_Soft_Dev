95d587532de4537894b884f6a32ca103825abf78
Christopher <clouie50@stuy.edu>
2024-11-07 02:57:36 -0500

    cleaned up

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-
 import os
 from build_db import *
 from flask import Flask, render_template, request, session, redirect
@@ -11,65 +10,43 @@ app.secret_key = secret
 
 @app.route("/")
 def disp_homepage():
-    # Fetch random entries to display, only keep valid entries
     rentry1 = getRandomEntry()
-    if not rentry1[0]:  # If no valid entry found
+    if not rentry1[0]:
         rentry1 = None
-    
     rentry2 = getRandomEntry()
-    if not rentry2[0]:  # If no valid entry found
+    if not rentry2[0]:
         rentry2 = None
-    
     rentry3 = getRandomEntry()
-    if not rentry3[0]:  # If no valid entry found
+    if not rentry3[0]:
         rentry3 = None
-    
     rentry4 = getRandomEntry()
-    if not rentry4[0]:  # If no valid entry found
+    if not rentry4[0]:
         rentry4 = None
-    
     rentry5 = getRandomEntry()
-    if not rentry5[0]:  # If no valid entry found
+    if not rentry5[0]:
         rentry5 = None
-
-    # Prepare the entries to pass to the template
     entries = [rentry1, rentry2, rentry3, rentry4, rentry5]
-    entries = [entry for entry in entries if entry is not None]  # Remove None entries
-
-    # Default values for blog-related data
-    myTitle, myBlogname, myText, myDate = "", "", "", ""  # Default values
-    uname = ""  # Ensure uname is always defined
-
-    # Check if logged in
-    if 'username' in session:
+    entries = [entry for entry in entries if entry is not None]  #Remove Nones
+    myTitle, myBlogname, myText, myDate = "", "", "", ""
+    uname = "" 
+    if 'username' in session: #Checks if logged in
         logged = True
-        uname = session['username']  # Assign uname if logged in
+        uname = session['username']  #Displays username
         myEntry = getMostRecentEntry(uname)
-        
-        # Handle case when the user has no entries
-        if myEntry:
+        if myEntry: #Check if User has entries
             myBlogname, myTitle, myText, myDate = myEntry
         else:
-            myBlogname, myTitle, myText, myDate = "", "", "You have no entries yet.", ""  # Default message when no entries
+            myBlogname, myTitle, myText, myDate = "", "", "You have no entries yet.", ""  #If no entries
     else:
         logged = False
-    
-    return render_template("homepage.html", 
-                           myTitle=myTitle, 
-                           entries=entries, 
-                           no_entries_message=myText, 
-                           logged=logged, 
-                           uname=uname)
-
-
-
+    return render_template("homepage.html",myTitle=myTitle,entries=entries,no_entries_message=myText,logged=logged,uname=uname)
 
 @app.route("/login")
 def disp_loginpage():
-    return render_template('login.html')
+    return render_template('login.html') #Login Page Rendering
 
 @app.route("/auth", methods=['POST'])
-def authenticate():
+def authenticate():#Is called when the user tries to log into their account
     username = request.form.get('username')
     password = request.form.get('password')
     stored_password = getPass(username)
@@ -78,38 +55,33 @@ def authenticate():
         return redirect("/")
     return render_template('login.html', error="Invalid username or password")
 
-
-@app.route("/create", methods=['GET', 'POST'])
+@app.route("/create", methods=['GET', 'POST']) #For when user tries to create an account witht their own blog
 def signup():
-    if request.method == 'POST':  # Check if the form was submitted
+    if request.method == 'POST':#Checks fields were filled out
         username = request.form.get('username')
         password = request.form.get('password')
-
         if username and password:
-            existing_user = getPass(username)  # Check if the user already exists
+            existing_user = getPass(username)#Check if the user already exists
             if existing_user:
                 return render_template('create.html', error="Username already exists") 
-            addUser(username, password)  # Add new user to the database
-            session['username'] = username  # Log the user in automatically
-            return redirect("/")  # Redirect to the homepage after successful signup
+            addUser(username, password)#Add new user to the database
+            session['username'] = username
+            return redirect("/")#Redirect to the homepage after successful signup
         else:
             return render_template('create.html', error="Please provide both a username and a password")
-    
-    # Render the signup page on GET request
-    return render_template('create.html')
+    return render_template('create.html')#Renders signup page at start
 
 @app.route("/logout")
 def logout():
     session.pop('username', None)
-    return render_template('logout.html')
+    return render_template('logout.html')#Similar to login page, just removes session
 
-@app.route("/thisBlog")
+@app.route("/thisBlog")#used to display blogs. All blog displays follow this format.
 def thisBlog():
     thisTitle = request.args.get('title')
     thisEntry = getEntry(thisTitle)
     blogname, entry, date = thisEntry
     return render_template('thisBlog.html', bname=blogname, dat=date, Title=thisTitle, txt=entry)
-
 
 @app.route("/edit", methods=['GET', 'POST'])
 def edit_post():
@@ -117,44 +89,36 @@ def edit_post():
         newTitle = request.form.get('newTitle')
         newText = request.form.get('newText')
         newDate = request.form.get('newDate')
-
         if newTitle and newText and newDate:
-            # Add the new post to the blog
-            bname = session.get('username', 'example_blog_name')  # Use the logged-in user's name as blog name
+            bname = session.get('username', 'example_blog_name') #Example blog takes the user's username for an example
             addEntry(bname, newTitle, newText, newDate)
-
             return redirect(f"/thisBlog?title={newTitle}")
         else:
             return render_template('edit.html', error="Please fill in all fields.")
-    
-    # For GET request, check if title is passed to edit an existing post
-    title = request.args.get('title')
+    title = request.args.get('title')#Check if title is passed to edit existing post
     if title:
         thisEntry = getEntry(title)
         blogname, entry, date = thisEntry
         return render_template('edit.html', bname=session.get('username', 'example_blog_name'), dat=date, currentTitle=title, currentText=entry, currentDate=date)
-
-    # If no title, render the form for creating a new entry
+    # If no title, render creating new entry
     return render_template('edit.html', bname=session.get('username', 'example_blog_name'), dat='2024-11-07')
 
+'''
 @app.route("/submit", methods=['GET', 'POST'])
 def submitEntry():
     nextTitle = request.form.get('newTitle')
     nextText = request.form.get('newText')
     nextDate = request.form.get('newDate')
-
     if nextTitle and nextText and nextDate:
         # Fetch most recent entry details
         myEntry = getMostRecentEntry(session['username'])
         myBlogname, myTitle, myText, myDate = myEntry
-
         # Add the new entry
         addEntry(myBlogname, nextTitle, nextText, nextDate)
-
         return render_template('thisBlog.html', bname=myBlogname, dat=nextDate, Title=nextTitle, txt=nextText)
-
     # If any required fields are missing, return to the form with an error
     return render_template('thisBlog.html', error="Please fill in all fields.")
+'''
 
 if __name__ == "__main__":
     app.debug = True
