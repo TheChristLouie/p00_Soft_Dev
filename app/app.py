63ab8450dcb0ba6c112a1b1ee235ab7c155e026f
Christopher <clouie50@stuy.edu>
2024-11-07 01:02:53 -0500

    updated to show name

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import os
 from build_db import *
 from flask import Flask, render_template, request, session, redirect
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitate CSV I/O
+import sqlite3   # Enable control of an sqlite database
+import csv       # Facilitate CSV I/O
 
 app = Flask(__name__)
 secret = os.urandom(32)
@@ -30,16 +30,15 @@ def disp_homepage():
     myTitle, myBlogname, myText, myDate = "", "", "", ""  # Default values
 
     # Check if logged in
+    logged = False
+    uname = ""
     if 'username' in session:
         logged = True
         uname = session['username']
         myEntry = getMostRecentEntry(uname)
         if myEntry:
             myBlogname, myTitle, myText, myDate = myEntry  # Unpack the latest entry if it exists
-    else:
-        logged = False
-    return render_template("homepage.html", myTitle=myTitle, title1=title1, title2=title2, title3=title3, title4=title4, title5=title5)
-
+    return render_template("homepage.html", logged=logged, uname=uname, myTitle=myTitle,title1=title1, title2=title2, title3=title3, title4=title4, title5=title5)
 
 @app.route("/login")
 def disp_loginpage():
@@ -56,16 +55,19 @@ def signup():
     if request.method == 'POST':  # Check if the form was submitted
         username = request.form.get('username')
         password = request.form.get('password')
+
         if username and password:
-            existing_user = getPass(username)
+            existing_user = getPass(username)  # Check if the user already exists
             if existing_user:
                 return render_template('create.html', error="Username already exists") 
-            addUser(username, password)
-            session['username'] = username
+            addUser(username, password)  # Add new user to the database
+            session['username'] = username  # Log the user in automatically
             return redirect("/")  # Redirect to the homepage after successful signup
         else:
             return render_template('create.html', error="Please provide both a username and a password")
-    return render_template('create.html')  # Render the sign-up page on GET request
+    
+    # Render the signup page on GET request
+    return render_template('create.html')
 
 @app.route("/logout")
 def logout():
@@ -80,23 +82,23 @@ def thisBlog():
     return render_template('thisBlog.html', bname=blogname, dat=date, Title=thisTitle, txt=entry)
 
 @app.route("/edit", methods=['GET', 'POST'])
-def create_post():
+def edit_post():
     if request.method == 'POST':
         newTitle = request.form.get('newTitle')
         newText = request.form.get('newText')
         newDate = request.form.get('newDate')
 
-        # Assuming bname is passed as part of the blog (you may want to update this logic)
-        bname = 'example_blog_name'  # You can dynamically pass the blog name here
+        if newTitle and newText and newDate:
+            # Add the new post to the blog
+            bname = session.get('username', 'example_blog_name')  # Use the logged-in user's name as blog name
+            addEntry(bname, newTitle, newText, newDate)
 
-        # Add new entry to the database
-        addEntry(bname, newTitle, newText, newDate)
-
-        # Redirect to the newly created blog post page (or the blog overview page)
-        return redirect(f"/thisBlog?title={newTitle}")
+            return redirect(f"/thisBlog?title={newTitle}")
+        else:
+            return render_template('edit.html', error="Please fill in all fields.")
     
-    # Render the page when visiting /create (GET request)
-    return render_template('edit.html', bname='example_blog_name', dat='2024-11-07')
+    # For GET request, render the edit form (no post data yet)
+    return render_template('edit.html', bname=session.get('username', 'example_blog_name'), dat='2024-11-07')
 
 @app.route("/submit", methods=['GET', 'POST'])
 def submitEntry():
